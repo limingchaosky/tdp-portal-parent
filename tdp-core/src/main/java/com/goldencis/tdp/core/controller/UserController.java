acf6c90051b4e4f6e4cb714a441a1ef5a648ce60
limingchaosky <limingchaosky@163.com>
2018-09-18 09:23:00 +0800

    Initial commit

---
+++ tdp-core/src/main/java/com/goldencis/tdp/core/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.goldencis.tdp.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+/**
+ * Created by limingchao on 2018/9/14.
+ */
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @RequestMapping(value = "/index")
+    public String indexPage(Model model) {
+        System.out.println("visit index site!");
+
+        model.addAttribute("tdpTime", new Date());
+        return "index";
+    }
+
+}
---
+++ tdp-webroot/src/main/java/com/goldencis/tdp/TdpApplication.java
@@ -0,0 +1,15 @@
+package com.goldencis.tdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by limingchao on 2018/9/14.
+ */
+@SpringBootApplication
+public class TdpApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(TdpApplication.class, args);
+    }
+}
